3c8839e36220148b16e7d456e2c0e22ede907be3
Daniel Tokkozhin <daniel@interspace.studio>
2020-06-14 15:38:49 +0600

    New experimental Flex Neomorph/Shadow components

---
+++ src/NeomorphFlex.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, ViewPropTypes } from 'react-native';
+import Neomorph from './Neomorph';
+
+export default class NeomorphFlex extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    const { style } = props;
+    this.state = {
+      neomorphStyle: {
+        shadowOffset: style?.shadowOffset,
+        shadowOpacity: style?.shadowOpacity,
+        shadowRadius: style?.shadowRadius,
+        borderRadius: style?.borderRadius,
+        backgroundColor: style?.backgroundColor,
+        width: 0,
+        height: 0,
+        position: 'absolute',
+      },
+      layoutFinished: false,
+    };
+  }
+
+  _onLayout = ({ nativeEvent }) => {
+    const { width, height } = nativeEvent.layout;
+    console.log(nativeEvent.layout);
+    this.setState({
+      neomorphStyle: {
+        ...this.state.neomorphStyle,
+        width,
+        height,
+      },
+      layoutFinished: true,
+    });
+  };
+
+  render() {
+    const { style, children, ...otherProps } = this.props;
+    const { layoutFinished, neomorphStyle } = this.state;
+    const {
+      elevation,
+      shadowOpacity,
+      shadowRadius,
+      shadowOffset,
+      overflow,
+      backgroundColor,
+      ...otherStyle
+    } = style;
+    return (
+      <View onLayout={this._onLayout} style={otherStyle}>
+        {layoutFinished && <Neomorph style={neomorphStyle} {...otherProps} />}
+        {children}
+      </View>
+    );
+  }
+}
+
+NeomorphFlex.propTypes = {
+  ...ViewPropTypes,
+  inner: PropTypes.bool,
+  useArt: PropTypes.bool,
+  darkShadowColor: PropTypes.string,
+  lightShadowColor: PropTypes.string,
+  swapShadows: PropTypes.bool,
+  children: PropTypes.node,
+};
---
+++ src/ShadowFlex.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Platform, ViewPropTypes } from 'react-native';
+import Shadow from './Shadow';
+
+export default class ShadowFlex extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    const { style } = props;
+    this.state = {
+      shadowStyle: {
+        shadowOffset: style?.shadowOffset,
+        shadowOpacity: style?.shadowOpacity,
+        shadowRadius: style?.shadowRadius,
+        shadowColor: style?.shadowColor,
+        borderRadius: style?.borderRadius,
+        backgroundColor: style?.backgroundColor,
+        width: 0,
+        height: 0,
+        position: 'absolute',
+      },
+      layoutFinished: false,
+    };
+  }
+
+  _onLayout = ({ nativeEvent }) => {
+    const { width, height } = nativeEvent.layout;
+    console.log(nativeEvent.layout);
+    this.setState({
+      shadowStyle: {
+        ...this.state.shadowStyle,
+        width,
+        height,
+      },
+      layoutFinished: true,
+    });
+  };
+
+  render() {
+    const { style, children, ...otherProps } = this.props;
+    if (Platform.OS === 'android') {
+      const { layoutFinished, shadowStyle } = this.state;
+      const {
+        elevation,
+        shadowOpacity,
+        shadowRadius,
+        shadowOffset,
+        shadowColor,
+        backgroundColor,
+        overflow,
+        ...otherStyle
+      } = style;
+      return (
+        <View onLayout={this._onLayout} style={otherStyle}>
+          {layoutFinished && <Shadow style={shadowStyle} {...otherProps} />}
+          {children}
+        </View>
+      );
+    } else {
+      return (
+        <Shadow style={style} {...otherProps}>
+          {children}
+        </Shadow>
+      );
+    }
+  }
+}
+
+ShadowFlex.propTypes = {
+  ...ViewPropTypes,
+  inner: PropTypes.bool,
+  useArt: PropTypes.bool,
+  children: PropTypes.node,
+};
