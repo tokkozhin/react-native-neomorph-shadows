8de794e18d7990d59f35b235a1dd73cd6d005e1f
Daniel Tokkozhin <daniel@interspace.studio>
2020-06-17 12:31:06 +0600

    export shadowFlex, neomorphFlex

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import Shadow from './src/Shadow';
 import Neomorph from './src/Neomorph';
 import NeomorphBlur from './src/NeomorphBlur';
-export { Shadow, Neomorph, NeomorphBlur };
+import ShadowFlex from './src/ShadowFlex';
+import NeomorphFlex from './src/NeomorphFlex';
+export { Shadow, Neomorph, NeomorphBlur, ShadowFlex, NeomorphFlex };
